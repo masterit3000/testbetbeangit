d601d3df6b2ef124e7e0a2281092e2ad8b5544c1
macbook162 <macbook162@DESKTOP-ADUE4QT>
2016-03-27 19:36:32 +0700

    vvvvvvvvvvvvv

---
+++ AnagramGame222/src/com/toy/anagrams/lib/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.toy.anagrams.lib;
+
+/**
+ *
+ * @author macbook162
+ */
+public class NewClass {
+    
+    public static void main(String[] args) {
+        System.out.println("sssssssssss");
+    }
+    
+}
